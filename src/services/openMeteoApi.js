90e1a5c1f852bda24c8b51a7fdcf70a1483988db
Christian Bechstedt <chrisnebech@hotmail.com>
2024-09-25 18:23:43 -0300

    fetch open meteo api

---
+++ src/services/openMeteoApi.js
@@ -0,0 +1,76 @@
+const marineWeatherDaily = async (lat, lon) => {
+  try {
+    const url = `https://marine-api.open-meteo.com/v1/marine?latitude=${lat}&longitude=${lon}&daily=wave_height_max,wave_direction_dominant,wave_period_max&timezone=America%2FSao_Paulo`;
+    
+    const response = await fetch(url);
+    
+    // Verifica se a resposta foi bem sucedida
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    // Verifica se o campo 'daily' existe na resposta
+    if (!data.daily) {
+      throw new Error('Dados diários não disponíveis');
+    }
+
+    const { wave_height_max, wave_direction_dominant, wave_period_max, time } = data.daily;
+
+    // Validação adicional para garantir que os dados existam
+    if (!wave_height_max || !wave_direction_dominant || !wave_period_max || !time) {
+      throw new Error('Dados incompletos retornados pela API');
+    }
+
+    return {
+      waveHeightMax: wave_height_max,
+      waveDirectionDominant: wave_direction_dominant,
+      wavePeriodMax: wave_period_max,
+      time
+    };
+
+  } catch (error) {
+    console.error('Erro ao buscar os dados da API:', error);
+    return null;
+  }
+};
+
+const marineWeatherCurrrent = async (lat, lon) => {
+  try {
+    const url = `https://marine-api.open-meteo.com/v1/marine?latitude=${lat}&longitude=${lon}&current=wave_height,wave_direction,wave_period&timezone=America%2FSao_Paulo`;
+    
+    const response = await fetch(url);
+    
+    // Verifica se a resposta foi bem sucedida
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    // Verifica se o campo 'current' existe na resposta
+    if (!data.current) {
+      throw new Error('Dados da corrente não disponíveis');
+    }
+
+    const { time, wave_height, wave_direction, wave_period } = data.current;
+
+    // Validação adicional para garantir que os dados existam
+    if (!wave_height || !wave_direction || !time || !wave_period) {
+      throw new Error('Dados incompletos retornados pela API');
+    }
+
+    return {
+      waveHeight: wave_height,
+      waveDirection: wave_direction,
+      wavePeriod: wave_period,
+      time
+    };
+
+  } catch (error) {
+    console.error('Erro ao buscar os dados da API:', error);
+    return null;
+  }
+};
+
