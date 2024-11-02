df04cb82b7df91fb69d326c20fb404a8402e2a24
Christian Bechstedt <chrisnebech@hotmail.com>
2024-11-02 00:03:38 -0300

    allow map to drag and zoom

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -12,7 +12,7 @@ export const Maps = () => {
 
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <div style={{ height: '300px', width: '300px'}}>
-        <Map zoom={14} center={postion} mapId={process.env.NEXT_PUBLIC_MAP_ID} />
+        <Map defaultZoom={14} defaultCenter={postion} mapId={process.env.NEXT_PUBLIC_MAP_ID} />
       </div>
     </APIProvider>
   )
